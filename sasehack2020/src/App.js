593f13d8347994c8e64589f7f03f4e9874808675
Dan Nguyens <danlily231.com>
2020-10-03 19:26:12 -0400

    chatbot

--- sasehack2020/src/App.js
+++ sasehack2020/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Sidebar from "./Sidebar/Sidebar";
+import Chatbot from "./Chatbot/Chatbot";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      <Chatbot />
     </div>
   );
 }
